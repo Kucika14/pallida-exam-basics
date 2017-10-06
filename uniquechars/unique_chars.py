a948c117893d6fcb93d31bcd537f48f3b873002c
Kucika14 <mate.akos@hotmail.com>
2017-10-06 10:23:30 +0200

    feat(unique_chars.py)just started

--- uniquechars/unique_chars.py
+++ uniquechars/unique_chars.py
@@ -1,6 +1,15 @@
 # Create a function called `unique_characters` that takes a string as parameter
 # and returns a list with the unique letters of the given string
 # Create basic unit tests for it with at least 3 different test cases
-print(unique_characters("anagram"))
 # Should print out:
 # ["n", "g", "r", "m"]
+
+def unique_characters(string):
+    list_of_letters = []
+    for i in string:
+        if i != "a":
+            list_of_letters.append(i)
+    return list_of_letters
+
+
+print(unique_characters("anagram"))
